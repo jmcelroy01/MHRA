928151fc477371f6d6bbd990af9845eb7b5b811d
Joseph McElroy <59029169+jmcelroy01@users.noreply.github.com>
2024-12-18 19:44:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ document.getElementById("convertButton").addEventListener("click", function () {
 
   // Function to convert MLA to MHRA
   function mlaToMhra(mlaText) {
-    const mlaPattern = /^([^,]+), ([^\.]+)\. "([^"]+)"\ ([^0-9]+) (\d+\.\d+) \((\d{4})\): (\d+)\.$/gm;
-    return mlaText.replace(mlaPattern, (_, lastName, firstName, title, journal, volumeIssue, year, page) => {
-      return `${firstName} ${lastName}, "${title}" (${journal}: ${year}), ${page}.`;
+    const mlaPattern = /^([^,]+), ([^\.]+)\. "([^"]+)"\ ([^0-9]+) \((\d{4})\): (\d+)\.$/gm;
+    return mlaText.replace(mlaPattern, (_, lastName, firstName, title, publisher, year, page) => {
+      return `${firstName} ${lastName}, ${title} (${publisher}: ${year}), pp. ${page}.`;
     });
   }
 
